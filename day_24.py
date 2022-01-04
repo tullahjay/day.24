1b3c19b91936279a268ff5dfebf1ebaa61ca3314
tullahjay <97083641+tullahjay@users.noreply.github.com>
2022-01-04 14:15:10 +0700

    Add files via upload

---
+++ day_24.py
@@ -0,0 +1,11 @@
+#menghapus item dari list menggunakan pop()
+list_nama = ['feri', 'hidaya', 'tullah', 'maulana', 'malik']
+print (list_nama)
+
+#hapus satu nama di belakang
+nama_yang_terhapus = list_nama.pop()
+print(list_nama)
+
+#hapus satu nama di depan
+nama_yang_terhapus = list_nama.remove('tullah')
+print(list_nama)
